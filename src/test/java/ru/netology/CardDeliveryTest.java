7f7e8710d978629a130c977ed0b2e0c4ab3c9dc2
AlarakMortiferum <sadim1403@gmail.com>
2025-05-30 18:58:43 +0300

    Версия 2

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,43 @@
+package ru.netology;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class CardDeliveryTest {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.headless = true; // запускать браузер без GUI
+        Configuration.browserSize = "1280x800"; // размер окна
+        Configuration.timeout = 5000; // таймаут ожидания по умолчанию
+    }
+
+    @Test
+    void shouldSubmitRequestSuccessfully() {
+        open("http://localhost:9999");
+
+        // дата встречи через 3 дня от текущей
+        String planningDate = LocalDate.now()
+                .plusDays(3)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $$("button").findBy(text("Забронировать")).click();
+
+        $("[data-test-id=notification]")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Встреча успешно забронирована на " + planningDate));
+    }
+}
